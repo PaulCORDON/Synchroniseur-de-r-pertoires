18ffc194fee5a5836faea9570fbba5d9a0a6852c
Unknown <antoine35.colin@gmail.com>
2017-12-15 10:59:30 +0100

    Obtention d'infos répertoire

--- ProjetSynchroRep/src/Client.java
+++ ProjetSynchroRep/src/Client.java
@@ -7,6 +7,12 @@ import java.io.OutputStreamWriter;
 import java.io.PrintWriter;
 import java.net.Socket;
 import java.net.UnknownHostException;
+import java.nio.*;
+import java.nio.file.DirectoryStream;
+import java.nio.file.FileSystems;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.Scanner;
 
 public class Client {
@@ -14,6 +20,8 @@ public class Client {
 	private String id;
 	private static int    _port;
 	private static Socket _socket;
+	private String repository = "H:/Mes documents/4A";
+	
 	
 	Client(String st){
 		id=st;
@@ -22,6 +30,8 @@ public class Client {
 	
 	
 	public static void main(String[] args) throws InterruptedException{	
+		
+		//new Client("50").recep(0);
 		
 		Scanner sc=new Scanner(System.in);
 		System.out.println("Quel est votre ID?");
@@ -68,6 +78,35 @@ public class Client {
 			{
 				e.printStackTrace();
 			}
+		}
+	}
+	
+	public void recep(int mode){
+		try{
+			//Path chemin = Paths.get(repository);
+			
+			Path roots = FileSystems.getDefault().getPath(repository);
+			//Maintenant, il ne nous reste plus qu'à parcourir
+			
+			  System.out.println(roots);
+			  //Pour lister un répertoire, il faut utiliser l'objet DirectoryStream
+			  //L'objet Files permet de créer ce type d'objet afin de pouvoir l'utiliser
+			  try(DirectoryStream<Path> listing = Files.newDirectoryStream(roots)){
+			    int i = 0;
+			    for(Path nom : listing){
+			    	if(Files.size(nom)>0){
+			      System.out.print("\t\t" + ((Files.isDirectory(nom)) ? nom+"/" : nom));
+			      System.out.println(" ["+Files.size(nom)+" octets]");
+			      System.out.print(" ,last modification : "+Files.getLastModifiedTime(nom)+"\n");
+			      i++;
+			      if(i%4 == 0)System.out.println("\n");
+			      }
+			    }
+			  } catch (IOException e) {
+			    e.printStackTrace();
+			  }
+		}catch(Exception e){
+			e.printStackTrace();
 		}
 	}
 }
