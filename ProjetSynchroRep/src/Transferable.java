899b93be4aedb40f46cb0691128f1142a28bc3c1
Unknown <antoine35.colin@gmail.com>
2018-01-08 15:31:10 +0100

    Auto stash before merge of "master" and "origin/master"

--- ProjetSynchroRep/src/Transferable.java
+++ ProjetSynchroRep/src/Transferable.java
@@ -132,38 +132,4 @@ public class Transferable {
 			}
 		}
 	}
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	public static void transfert(InputStream in, OutputStream out, boolean closeOnExit) throws IOException  {
-		  
-        byte buf[] = new byte[1024];
-        
-        int n;
-        while((n=in.read(buf))!=-1) {
-        	out.write(buf,0,n);
-        }
-            
-        
-        if (closeOnExit){
-            in.close();
-            out.close();
-        }
-    }
-	
-	
-	
-	
-	
-	
-	
-	
 }
